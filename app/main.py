2dd77e2f6aa340863977d1e95705873aadd770b8
Siva-0310 <sivachadalawada0310@gmail.com>
2023-11-03 09:16:28 +0530

    added 404 html

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 import pickle
 import uvicorn
-from fastapi import FastAPI,HTTPException,status
+from fastapi import FastAPI,HTTPException,status,Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from schemas import Data,PredictOutput
 
 app = FastAPI()
 MODEL = pickle.load(open("model.pkl","rb"))
+
+templates = Jinja2Templates(directory="templates")
+
 
 @app.post("/model",response_model=PredictOutput)
 def predict(body:Data):
@@ -13,5 +18,14 @@ def predict(body:Data):
     except:
         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR,detail="server error")
     return {"ans":int(res[0])}
+
+@app.exception_handler(HTTPException)
+async def custom_404(request:Request,exec):
+    return templates.TemplateResponse("404.html",{"request":request},status_code=404)
+
+@app.get("/{path:path}")
+async def not_found(path: str):
+    raise HTTPException(status_code=404, detail="Not Found")
+
 if __name__ == "__main__":
     uvicorn.run(app="main:app",port=8080,reload=True)
